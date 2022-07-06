209a5b03e27d9b84ea923255a7e76e46717fd29f
tablopik <65659439+1cixin@users.noreply.github.com>
2022-07-06 11:44:03 +0800

    Create 删除重复元素.cpp

---
+++ 顺序表/删除重复元素.cpp
@@ -0,0 +1,11 @@
+有序顺序表删除所有重复元素
+void OnlyElem(lqList &L) {
+        int k=0;
+        for(int i=0;i<L.length-1;i++){
+            if(L.data[i]==L.data[i+1])
+                k++;
+            else
+                L.data[i-k]=L.data[i];
+        }
+        L.length-=k;
+    }
