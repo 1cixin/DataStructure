c2522af69d57357e7f3c10b0396d11e5b6453cde
tablopik <65659439+1cixin@users.noreply.github.com>
2022-07-21 11:12:49 +0800

    Update 最小栈.cpp

--- 线性表/栈/最小栈.cpp
+++ 线性表/栈/最小栈.cpp
@@ -14,7 +14,7 @@
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
-//解法一：（手撕）栈同时存储（最小值+数据值）
+//解法一：（手撕顺序栈）栈同时存储（最小值+数据值）
 #define MaxSize 20000
 typedef struct{
     int data[MaxSize];      //存放栈中元素
@@ -69,7 +69,59 @@ public:
  * int param_4 = obj->getMin();
  */
 
-//解法二：双栈（数据栈 + 辅助栈）
+//解法二：（手撕链栈）栈同时存储（最小值+数据值）
+typedef struct Listnode{
+    int data;               //数据域
+    int min;
+    struct Listnode *next;  //指针域
+    Listnode() : data(INT_MAX), min(INT_MAX), next(nullptr) {}
+    Listnode(int x) : data(x), next(nullptr) {}
+}Listnode, *Lnode;                //栈类型定义
+
+
+class MinStack {
+    Listnode* head = new Listnode();             //全局变量
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        Listnode *p = new Listnode(val);
+        head->min = val < head->min ? val : head->min;  //head->min用来比较
+        p->min = head->min;                             //p->min用来getMin
+        p->next = head->next;
+        head->next = p;
+    }
+    
+    void pop() {
+        if(!head->next)
+            return;
+        Listnode *s = head->next;
+        head->next = head->next->next;
+        delete s;
+        head->min = head->next ? head->next->min : INT_MAX;
+    }
+    
+    int top() {
+        return head->next->data;
+    }
+    
+    int getMin() {
+        return head->next->min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+//解法三：双栈（数据栈 + 辅助栈）
 class MinStack {
     stack<int> x_stack;
     stack<int> min_stack;
