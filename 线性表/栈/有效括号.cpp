fdb8122b76ec2b099420af7cc9d932e0269ff423
tablopik <65659439+1cixin@users.noreply.github.com>
2022-08-08 22:21:08 +0800

    Update 有效括号.cpp

--- 线性表/栈/有效括号.cpp
+++ 线性表/栈/有效括号.cpp
@@ -12,6 +12,8 @@
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
 
+
+解法一：map匹配
 class Solution {
 public:
     bool isValid(string s) {
@@ -38,3 +40,38 @@ public:
         return stk.empty();
     }
 };
+
+解法二switch：
+bool BracketsCheck(char * str){
+    InitStack(S);
+    int i=0;
+    while(str[i]!='\0'){
+        switch(str[i]){
+            //左括号入栈
+            case '(': Push(S,'(');  break;
+            case '[': Push(S,'[');  break;
+            case '{': Push(S,'{');  break;
+            //遇到右括号，检测栈顶
+            case ')': Pop(S,e);
+                if(e!='(')  return false;
+            break;
+            case ']': Pop(S,e);
+                if(e!='[')  return false;
+            break;
+            case '}': Pop(S,e);
+                if(e!='{')  return false;
+            break;
+            default:
+            break;
+        }//switch
+        i++;
+    }//while
+    if(!IsEmpty(S)){
+        printf("括号不匹配\n");
+        return false;
+    }
+    else{
+        printf("括号匹配\n");
+        return true;
+    }
+}
